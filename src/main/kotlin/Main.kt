e548ef77d6eac00a318fb7ca6d8bc0f60fb95da1
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-21 08:50:06 -0300

    Add class Funcionario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 fun main(args: Array<String>) {
     println("Bem vindo ao Bytebank!")
 
-    testaContas()
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.222.333-45",
+        salario = 1000.0
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificacao ${alex.bonificacao()}")
 
 }
 
