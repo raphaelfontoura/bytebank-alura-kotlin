e029396bc5f7a939cc7b299e8cd6a02e80fb2d69
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-21 10:42:21 -0300

    Use key word super to call bonificacao Funcionario

--- src/main/kotlin/Diretor.kt
+++ src/main/kotlin/Diretor.kt
@@ -11,7 +11,7 @@ class Diretor(
 ) {
     override val bonificacao: Double
         get() {
-            return salario * 0.3
+            return super.bonificacao + salario + plr
         }
 
     fun autenticar(senha: String): Boolean {
