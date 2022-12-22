79f81c6392acf8b31cfd964c2cbde080aa69219f
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-22 19:52:11 -0300

    Adicionado o companion object como Contador no Conta
    Para criar uma variável estática (static) com um valor que será único
    para todas as instâncias de contas, foi utilizado o recurso Object
    declarations do Kotlin.
    E para que a classe abstrata Conta possa ter acesso ao set privado, foi
    utilizado a palavra chave "companion" que torna este objeto anônimo
    acessível a Conta.
    https://kotlinlang.org/docs/object-declarations.html#semantic-difference-between-object-expressions-and-declarations

--- src/main/kotlin/br/com/rddev/bytebank/Main.kt
+++ src/main/kotlin/br/com/rddev/bytebank/Main.kt
@@ -1,10 +1,13 @@
 package br.com.rddev.bytebank
 
+import br.com.rddev.bytebank.model.Conta
 import br.com.rddev.bytebank.teste.testaContas
 
 fun main(args: Array<String>) {
 
     testaContas()
+
+    println("Total de contas: ${Conta.Contador.total}")
 
 }
 
--- src/main/kotlin/br/com/rddev/bytebank/model/Conta.kt
+++ src/main/kotlin/br/com/rddev/bytebank/model/Conta.kt
@@ -7,6 +7,14 @@ abstract class Conta(
 ) {
     var saldo = 0.0
         protected set
+    companion object Contador {
+        var total: Int = 0
+            private set
+    }
+    init {
+        println("Criando conta")
+        total++
+    }
 
     fun depositar(valor: Double) {
         if (valor > 0) this.saldo += valor
