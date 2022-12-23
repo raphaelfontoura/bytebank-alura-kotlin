715960ee7b817d344edbbfcb612e532da001db41
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-23 00:16:14 -0300

    Add equals and hashcode to Endereco

--- src/main/kotlin/br/com/rddev/bytebank/Main.kt
+++ src/main/kotlin/br/com/rddev/bytebank/Main.kt
@@ -4,14 +4,29 @@ import br.com.rddev.bytebank.model.Endereco
 
 fun main(args: Array<String>) {
 
-    imprime(Any())
-    imprime(1)
-    imprime(1.0)
-    imprime(Endereco())
+    val endereco = Endereco()
+    val enderecoNovo = Endereco()
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+    endereco.logradouro = "Rua Fernando de Noronha"
+    endereco.numero = 1005
+    endereco.bairro = "Nowhere"
+    endereco.cidade =  "Noone"
+    endereco.estado = "OO"
+    endereco.cep = "00000-000"
+
+    enderecoNovo.cep = "00000-000"
+
+    println(endereco.toString())
+    println(enderecoNovo.toString())
+
+    println(endereco.equals(enderecoNovo))
+
+    println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
+    println("${enderecoNovo.javaClass}@${Integer.toHexString(enderecoNovo.hashCode())}")
 
 }
 
-fun imprime(valor: Any) {
-    println(valor)
-}
 
